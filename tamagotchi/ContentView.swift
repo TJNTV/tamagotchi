bdb68d0d3fa88d2504917f1dc7eb7534be5ecaf3
Diplodocus <113530020+TJNTV@users.noreply.github.com>
2026-02-22 00:25:10 +0000

    Add files via upload

---
+++ tamagotchi/ContentView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  tamagotchi
+//
+//  Created by Toby Nash on 12/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    // Stores all notes
+    @State private var notes: [String] = []
+
+    // Stores the current text being typed
+    @State private var newNote: String = ""
+
+    var body: some View {
+        NavigationStack {
+            VStack {
+            }
+            .navigationTitle("Tamagotchi App 😎")
+            .navigationSubtitle("By Toby Nash")
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ tamagotchi/tamagotchiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tamagotchiApp.swift
+//  tamagotchi
+//
+//  Created by Toby Nash on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct tamagotchiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
